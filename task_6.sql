084add6f005449a58ac066d8f236ea82ff250f82
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-10-19 17:32:50 +0100

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- task_6.sql
 -- Script to insert multiple rows into the table 'customer'
 
-INSERT INTO customer (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
